16c53d98936776930ed181c2d0bb70bbda777059
Will <william.layne.richardson@gmail.com>
2024-11-11 16:27:32 -0700

    obstcal

--- src/game-objects/obsitcle.js
+++ src/game-objects/obsitcle.js
@@ -1,19 +1,27 @@
-import { HEIGHT, WIDTH } from "../constents";
+import { HEIGHT, WIDTH } from "../constants";
 
- export class obstical extends Phaser.GameObjects.Rectangle{
-    constructor(scene) {
-        super(scene,WIDTH + 100,Phaser.Math.Between(HEIGHT - 32,HEIGHT - 100),32,32,0xff0000);
+export class Obstacle extends Phaser.GameObjects.Rectangle {
+	constructor(scene) {
+		super(
+			scene,
+			WIDTH + 100,
+			Phaser.Math.Between(HEIGHT - 32, HEIGHT - 100),
+			32,
+			32,
+			0xff0000
+		);
 
-        this.scene.add.existing(this);
-        this.scene.physics.add.existing(this)
+		this.scene.add.existing(this);
+		this.scene.physics.add.existing(this);
+        this.body.setAllowGravity(false);
 
-        this.scene.tweens.add({
-            targets:this,
-            x:(from:WIDTH + 100,to: -100),
-            duration: 2000,
-            onComplete: () => {
-                this.destroy(),
-            }
-        });
-    };
+		this.scene.tweens.add({
+			targets: this,
+			x: { from: WIDTH + 100, to: -100 },
+			duration: 2000,
+			onComplete: () => {
+				this.destroy();
+			},
+		});
+	}
 }
